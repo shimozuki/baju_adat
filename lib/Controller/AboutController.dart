c6172868d5d247956397e6a8c11b2399aa88daa7
shimozuki <robbishimozuki@gmail.com>
2024-06-19 08:18:47 +0800

    master

--- lib/Controller/AboutController.dart
+++ lib/Controller/AboutController.dart
@@ -4,15 +4,28 @@ import 'package:pakaian_adat/model/about_model.dart';
 
 class AboutController {
   static Future<List<About_Model>> getAboutData() async {
-    final response = await http.get(
-      Uri.parse('https://musik-tradisionalsumbawa.com/api/get-about'),
-    );
+    const int maxRetries = 3;
+    int retryCount = 0;
+    while (retryCount < maxRetries) {
+      try {
+        final response = await http.get(
+          Uri.parse('https://musik-tradisionalsumbawa.com/api/get-about'),
+        );
 
-    if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body)['data'];
-      return data.map((json) => About_Model.fromJson(json)).toList();
-    } else {
-      throw Exception('Failed to load about data');
+        if (response.statusCode == 200) {
+          final List<dynamic> data = json.decode(response.body)['data'];
+          return data.map((json) => About_Model.fromJson(json)).toList();
+        } else {
+          throw Exception('Failed to load about data');
+        }
+      } catch (e) {
+        retryCount++;
+        if (retryCount == maxRetries) {
+          throw Exception(
+              'Failed to load about data after $maxRetries attempts: $e');
+        }
+      }
     }
+    return [];
   }
 }
